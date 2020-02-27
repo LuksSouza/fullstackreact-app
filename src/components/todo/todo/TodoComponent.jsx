bc9cd5c943158c8d707ac6e41dec638f6799baaa
Lucas Souza <lucas-machado@outlook.com>
2020-02-27 07:22:53 +0000

    Implemented form validation with validate method and formik <ErrorMessage>

--- src/components/todo/todo/TodoComponent.jsx
+++ src/components/todo/todo/TodoComponent.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 
 class Todo extends Component {
 
@@ -14,6 +14,23 @@ class Todo extends Component {
         };
 
         this.onSubmit = this.onSubmit.bind(this);
+        this.validade = this.validade.bind(this);
+    }
+
+    validade(values) {
+        let errors = {};
+
+        if (!values.description) {
+            errors.description = 'Enter a Description';
+        } else if (!values.description.length > 5) {
+            errors.description = 'Enter at least 5 characters in Description';
+        }
+
+        if (!moment(values.targetDate).isValid()) {
+            errors.targetDate = 'Enter a valid Target Date';
+        }
+
+        return errors;
     }
 
     onSubmit(values) {
@@ -27,13 +44,16 @@ class Todo extends Component {
             <div>
                 <h1>Todo</h1>
                 <div className="container">
-                    <Formik
-                        initialValues={{description, targetDate}}
+                    <Formik initialValues={{description, targetDate}}
                         onSubmit={this.onSubmit}
-                    >
+                        validateOnChange={false}
+                        validateOnBlur={false}
+                        validate={this.validade}>
                         {
                             (props) => (
                                 <Form>
+                                    <ErrorMessage name="description" component="div" className="alert alert-warning" />
+                                    <ErrorMessage name="targetDate" component="div" className="alert alert-warning" />
                                     <fieldset className="form-group">
                                         <label>Description</label>
                                         <Field className="form-control" type="text" name="description"></Field>
